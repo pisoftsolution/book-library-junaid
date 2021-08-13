0f804390fa23151097bd3d75efa3ac73f26bcd19
junaid <junaidali1507@gmail.com>
2021-08-13 18:37:21 +0530

    added files

--- routes/categories.js
+++ routes/categories.js
@@ -4,9 +4,9 @@ const books = require('../controllers/categories');
 
 router.post('/add-new-category' , books.addNewCategory);
 router.get('/get-category-by-id' , books.getCategoryById);
-router.get('/view-all-categories' ,books.getAllCategories);
-router.post('/add-Category' ,books.addCategory);
-router.put('/edit-category-by-id' ,books.editCategoryById);
+router.get('/view-all-categories' , books.getAllCategories);
+router.post('/add-Category' , books.addCategory);
+router.put('/edit-category-by-id' , books.editCategoryById);
 router.delete('/delete-category-by-id' , books.DeleteCategoryById);
 
 module.exports = router;
